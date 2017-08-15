519d156bba8d7ba1169deb29b08bea6053e383f5
Alan Tavares <alan1tavares@gmail.com>
2017-08-15 20:40:26 -0300

    Adicionados os scripts para criar as tabelas que faltavam.

--- app/infra/sqlScript.sql
+++ app/infra/sqlScript.sql
@@ -1,7 +1,65 @@
-create table usuario(
+-- Tabela aluna
+create table aluno(
     id int auto_increment primary key,
     nome varchar(32),
     matricula varchar(32),
     email varchar(32),
     senha varchar(32)
+);
+-- Tabela professor
+create table professor(
+    id int auto_increment,
+    nome varchar(32),
+    cpf int,
+    endereco varchar(64),
+    cep int,
+    numero int,
+    telefone int,
+    email varchar(32),
+    primary key (id)
+);
+-- Tabela instituição
+create table instituicao(
+    id int auto_increment,
+    nome varchar(62),
+    cnpj int,
+    endereco varchar(64),
+    cep int,
+    numero int,
+    telefone int,
+    primary key (id)
+);
+-- Tabela disciplina
+create table disciplina(
+    id int auto_increment,
+    nome varchar(32),
+    primary key (id)
+);
+-- Tabela turma
+create table turma(
+    id int auto_increment,
+    nome varchar(32),
+    primary key (id)
+);
+-- Tabela exercícios
+create table exercicios(
+    id int auto_increment,
+    titulo varchar(1000),
+    proposito varchar(100),
+    imagem varchar(32),
+    primary key(id)
+);
+-- Tabela prova
+create table prova(
+    id int auto_increment,
+    titulo varchar(1000),
+    proposito varchar(100),
+    imagem varchar(32),
+    primary key(id)
+);
+-- Tabela semestre
+create table semestre(
+    id int auto_increment,
+    nome varchar(32),
+    primary key(id)
 );
