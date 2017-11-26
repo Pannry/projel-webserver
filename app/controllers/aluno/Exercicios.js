0acd8fd5127f696341a73cbc033eed26edfb001f
Pannry <thalestnd@gmail.com>
2017-11-26 14:02:41 -0300

    Criação da resposta do aluno concluido

--- app/controllers/aluno/Exercicios.js
+++ app/controllers/aluno/Exercicios.js
@@ -8,12 +8,15 @@ module.exports = function ( app ) {
             if ( req.user.tipo == 'aluno' ) {
 
                 let ejs = {
-                    user: req.user, page_name: req.path, accountType: req.user.tipo,
-                    sala: req.params.id_sala, lista: req.params.id_lista
+                    user: req.user,
+                    page_name: req.path,
+                    accountType: req.user.tipo,
+                    sala: req.params.id_sala,
+                    lista: req.params.id_lista
                 }
 
                 let entrada = {
-                    id_sala: req.params.id_sala, id_lista: req.params.id_lista
+                    id_lista: req.params.id_lista,
                 }
 
                 let conexaoDb = app.infra.banco.dbConnection();
@@ -21,7 +24,8 @@ module.exports = function ( app ) {
 
                 ExerciciosDao.mostrarExerciciosAluno( entrada.id_lista, function ( err, resultado ) {
                     ejs.exercicios = resultado;
-                    res.render( 'aluno/perfil/exercicios/abrirListaAluno.ejs', ejs );
+                    res.render( 'aluno/perfil/exercicios/abrirListaAluno', ejs );
+
                 } );
                 conexaoDb.end();
             } else
@@ -35,35 +39,67 @@ module.exports = function ( app ) {
         get: function ( req, res ) {
             if ( req.user.tipo == 'aluno' ) {
 
-                let entrada = req.params.id_exercicio;
-
-                let ejs = {
-                    user: req.user, page_name: req.path, accountType: req.user.tipo,
-                    sala: req.params.id_sala, lista: req.params.id_lista, exercicio: req.params.id_exercicio
+                let entrada = {
+                    id_exercicios: req.params.id_exercicio,
+                    id_aluno: req.user.id,
+                    resposta: '',
+                    pdf_path: ''
                 }
 
-                let conexaoDb = app.infra.banco.dbConnection();
-                let ExerciciosDao = new app.infra.banco.ExerciciosDao( conexaoDb );
+                let ejs = {
+                    user: req.user,
+                    page_name: req.path,
+                    accountType: req.user.tipo,
+                    sala: req.params.id_sala,
+                    lista: req.params.id_lista,
+                    respostaAluno: ''
+                }
 
-                ExerciciosDao.mostrarExercicioUnicoAluno( entrada, function ( err, resultado ) {
-                    ejs.resultadoExercicio = resultado;
-                    res.render( 'aluno/perfil/exercicios/responderExercicio.ejs', ejs );
+                let conexaoDb3 = app.infra.banco.dbConnection();
+                let ExerciciosDao3 = new app.infra.banco.ExerciciosDao( conexaoDb3 );
+
+                ExerciciosDao3.abrirExercicio( entrada.id_exercicios, function ( err, resultado3 ) {
+                    ejs.exercicio = resultado3;
+
+                    let conexaoDb2 = app.infra.banco.dbConnection();
+                    let ExerciciosDao2 = new app.infra.banco.ExerciciosDao( conexaoDb2 );
+
+                    ExerciciosDao2.abrirRespostaAluno( entrada, function ( err, resultado2 ) {
+                        if ( resultado2.length === 0 ) {
+                            let conexaoDb1 = app.infra.banco.dbConnection();
+                            let ExerciciosDao1 = new app.infra.banco.ExerciciosDao( conexaoDb1 );
+
+                            ExerciciosDao1.criarResposta( entrada, function ( err, resultado ) {
+                                res.render( 'aluno/perfil/exercicios/responderExercicio', ejs );
+                            } );
+
+                            conexaoDb1.end();
+                        } else {
+                            ejs.respostaAluno = resultado2[ 0 ].resposta;
+                            res.render( 'aluno/perfil/exercicios/responderExercicio', ejs );
+                        }
+                    } );
+                    conexaoDb2.end();
                 } );
-                conexaoDb.end();
+                conexaoDb3.end();
+
 
             } else
                 res.status( 403 );
         },
+
         post: function ( req, res ) {
             let entrada = {
-                id_aluno: req.user.id, id_exercicios: req.params.id_exercicio, foto: ''
+                id_aluno: req.user.id,
+                id_exercicios: req.params.id_exercicio,
+                pdf_path: '',
+                resposta: req.body.resposta
             }
 
             ejs = {
-                sala: req.params.id_sala, lista: req.params.id_lista
+                sala: req.params.id_sala,
+                lista: req.params.id_lista
             }
-
-            entrada.resposta = req.body.resposta;
 
             let conexaoDb = app.infra.banco.dbConnection();
             let ExerciciosDao = new app.infra.banco.ExerciciosDao( conexaoDb );
