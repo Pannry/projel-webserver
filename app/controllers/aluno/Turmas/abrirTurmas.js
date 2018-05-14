0f4570dd4de5430b7fe90710dc29a57b14b0e8f6
Pannry <thalestnd@gmail.com>
2018-05-14 19:08:32 -0300

    testando o uso de promise

--- app/controllers/aluno/Turmas/abrirTurmas.js
+++ app/controllers/aluno/Turmas/abrirTurmas.js
@@ -15,33 +15,62 @@ module.exports = (app) => {
           accountType: req.user.tipo,
         };
 
-        const conexaoDb1 = app.infra.banco.dbConnection();
-        const salaDAO = new app.infra.banco.SalaDAO(conexaoDb1);
+        const firstMethod = function () {
+          const promise = new Promise((resolve) => {
+            const conexaoDb1 = app.infra.banco.dbConnection();
+            const salaDAO = new app.infra.banco.SalaDAO(conexaoDb1);
 
-        // Informações da sala
-        salaDAO.buscarSala(entrada1, (err, resultado1) => {
-          ejs.sala = resultado1;
-          entrada2.id_sala = resultado1[0].id;
+            salaDAO.buscarSala(entrada1, (err, resultado1) => {
+              if (err) throw (err);
+              ejs.sala = resultado1;
+              entrada2.id_sala = resultado1[0].id;
+              resolve();
+            });
+            conexaoDb1.end();
+          });
+          return promise;
+        };
 
-          const conexaoDb2 = app.infra.banco.dbConnection();
-          const salaDAO2 = new app.infra.banco.SalaDAO(conexaoDb2);
+        const secondMethod = function () {
+          const promise = new Promise((resolve) => {
+            const conexaoDb2 = app.infra.banco.dbConnection();
+            const salaDAO2 = new app.infra.banco.SalaDAO(conexaoDb2);
 
-          salaDAO2.verificarAutenticacao(entrada2, (err, resultado2) => {
-            ejs.aluno_aceito = resultado2[0].aluno_aceito;
+            salaDAO2.verificarAutenticacao(entrada2, (err2, resultado2) => {
+              ejs.aluno_aceito = resultado2[0].aluno_aceito;
+              resolve();
+            });
+            conexaoDb2.end();
+          });
+          return promise;
+        };
 
+        const thirdMethod = function () {
+          const promise = new Promise((resolve) => {
             const conexaoDb3 = app.infra.banco.dbConnection();
             const ExerciciosDao = new app.infra.banco.ExerciciosDao(conexaoDb3);
 
-            ExerciciosDao.mostrarListasAluno(entrada3, (err, resultado3) => {
+            ExerciciosDao.mostrarListasAluno(entrada3, (err3, resultado3) => {
               ejs.lista = resultado3;
-
-              res.render('aluno/perfil/turmas/abrirTurma', ejs);
+              resolve();
             });
             conexaoDb3.end();
           });
-          conexaoDb2.end();
-        });
-        conexaoDb1.end();
+          return promise;
+        };
+
+        const fourthMethod = function () {
+          const promise = new Promise(() => {
+            res.render('aluno/perfil/turmas/abrirTurma', ejs);
+          });
+          return promise;
+        };
+
+        firstMethod()
+          .then(secondMethod)
+          .then(thirdMethod)
+          .then(fourthMethod)
+          .catch((err) => { console.log(err); });
       }
     },
   };
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var app = require('./config/express')();
+const app = require('./config/express')();
 
 app.listen(app.get('port'), () => {
-    console.log(`servidor rodando na porta ${app.get('port')}`);
+  console.log(`servidor rodando na porta ${app.get('port')}`);
 });
