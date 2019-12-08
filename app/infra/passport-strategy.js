0bfdc478eab99212510bff22a59ec4981df00c31
Pannry <thalestnd@gmail.com>
2019-12-08 16:31:54 -0300

    Atualizando dependencias (dneovo)

--- app/infra/passport-strategy.js
+++ app/infra/passport-strategy.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 module.exports = (app) => {
   const passport = app.get('passport');
