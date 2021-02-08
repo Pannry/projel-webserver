6311a293e83c82298b5454c79bddf6015861a803
Pannry <thalestnd@gmail.com>
2021-02-07 22:20:20 -0300

    .

--- app/controllers/aluno/Classroom.js
+++ app/controllers/aluno/Classroom.js
@@ -69,6 +69,9 @@ exports.openClassrooms = asyncHandler(async (req, res, next) => {
     const getDidaticList = await studentL.showList(entrada[1].id_sala);
     ejs.didatico = getDidaticList;
 
+    console.log('=====ejs====');
+    console.log(ejs);
+
     res.render('aluno/perfil/turmas/abrirTurma', ejs);
   } else {
     next();
